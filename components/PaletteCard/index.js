9cfb04ca10cbc817b24b3fb10d0ccae8c4d1d760
sidchigo <sidbakayaro@gmail.com>
2022-01-02 14:54:26 +0530

    feat: golden ratio palette-gen completed

---
+++ components/PaletteCard/index.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+// components
+import { Colorcard } from "components/Colorcards/Colorcard";
+
+// utility
+import { paletteGenerator } from "utility/ColorGenerator";
+import { Button } from "components/Button";
+
+const PaletteCard = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        setData(paletteGenerator());
+    }, []);
+
+    const fetchMorePalettes = () => {
+        const newPalettes = paletteGenerator();
+        const newData = data.concat(newPalettes);
+        setData(newData);
+    }
+
+    return (
+		<>
+			<div className="mx-4 sm:mx-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-3 mb-3">
+				{data.length !== 0 &&
+					data.map((color, index) => <Colorcard key={index} colorData={color} />)}
+			</div>
+			<div className={`flex justify-center items-center my-8`}>
+				<Button
+					variant={`bg-violet-600 hover:bg-violet-700 text-white py-6 px-8`}
+					onClick={fetchMorePalettes}
+				>
+					Generate more!
+				</Button>
+			</div>
+		</>
+	);
+}
+
+export default PaletteCard;
