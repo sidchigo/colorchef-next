ad2eb43a4159260e18d0c463d022f80deddde6f9
sidchigo <sidbakayaro@gmail.com>
2022-11-02 09:03:19 +0530

    chore: add sidebar links

--- components/Layout/sidebar.jsx
+++ components/Layout/sidebar.jsx
@@ -1,10 +1,31 @@
+import Link from 'next/link';
 import styles from './sidebar.module.css';
+
+const ROUTES = [
+    { name: 'color generator', path: 'colors' },
+    { name: 'shadow generator', path: 'shadows' },
+    { name: 'dark palette', path: 'dark-palette' },
+    { name: 'buttons', path: 'buttons' },
+    { name: 'golden ratio', path: 'golden-ratio' },
+];
+
+const LinkButton = ({ children, href }) => {
+    return (
+        <a href={href}>{children}</a>
+    )
+}
 
 export const Sidebar = () => {
     return(
         <aside className={`col-span-1 flex flex-col text-center justify-between z-[3] ${styles.sidebar}`}>
             <div>Colorchef-logo</div>
-            <div>Menu</div>
+            <div className={`flex flex-col justify-around capitalize`}>
+                {ROUTES.map(route => (
+                    <Link href={route.path} passHref>
+                        <LinkButton>{route.name}</LinkButton>
+                    </Link>
+                ))}
+            </div>
             <div>About</div>
         </aside>
     )
