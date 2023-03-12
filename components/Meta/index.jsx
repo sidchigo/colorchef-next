fd8b40ea53740ab3677cc67f58be5b17a00ec0ad
sidchigo <sidbakayaro@gmail.com>
2023-03-12 11:56:44 +0530

    chore: try og api

--- components/Meta/index.jsx
+++ components/Meta/index.jsx
@@ -13,7 +13,7 @@ const Meta = ({ title, colorcode, image, description }) => {
             <meta property="og:type" content="website" />
             <meta
                 property="og:url"
-                content={`https://colorchef.vercel.app/api/og?color=${colorcode}`}
+                content={`https://colorchef.vercel.app/api/og?username=sidchigo`}
             />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
@@ -26,13 +26,13 @@ const Meta = ({ title, colorcode, image, description }) => {
             <meta property="twitter:card" content="summary_large_image" />
             <meta
                 property="twitter:url"
-                content={`https://colorchef.vercel.app/api/og?color=${colorcode}`}
+                content={`https://colorchef.vercel.app/api/og?username=sidchigo`}
             />
             <meta property="twitter:title" content={title} />
             <meta property="twitter:description" content={description} />
             <meta
                 property="twitter:image"
-                content={`https://colorchef.vercel.app/api/og?color=${colorcode}`}
+                content={`https://colorchef.vercel.app/api/og?username=sidchigo`}
             />
         </>
     );
