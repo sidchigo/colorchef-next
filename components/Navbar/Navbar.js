b6654d395bdd448a996b1dfad7796ae1522d495f
sidchigo <sidbakayaro@gmail.com>
2023-05-20 08:55:15 +0530

    chore: change in about descr and position

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,92 +1,103 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import styles from './Navbar.module.css';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
 
-import Burger from './Burger';
-import { Auth } from 'components/Auth';
+import Burger from "./Burger";
+import { Auth } from "components/Auth";
 
 // icons
-import logo from 'icons/logo.svg';
+import logo from "icons/logo.svg";
 
 // helpers
-import useOutsideAlerter from 'utility/OutsideClickHandler';
+import useOutsideAlerter from "utility/OutsideClickHandler";
 
 function Navbar() {
-	const { ref, show, setShow } = useOutsideAlerter(false);
-	const router = useRouter();
+    const { ref, show, setShow } = useOutsideAlerter(false);
+    const router = useRouter();
 
-	return (
-		<nav className="bg-white sticky top-0 w-full z-50 flex justify-center sm:justify-between items-center px-8 py-3 lg:py-3">
-			<Link href="/">
-				<a className="flex justify-self-center items-center">
-					<img src={logo} width="150" height="150" alt="Colorchef" />
-				</a>
-			</Link>
-			<div className="block lg:hidden" ref={ref}>
-				<Burger open={show} setOpen={setShow} />
-			</div>
-			<div className="hidden lg:flex justify-end">
-				<Link href="/colors">
-					<a
-						className={`
+    return (
+        <nav className="bg-white sticky top-0 w-full z-50 flex justify-center sm:justify-between items-center px-8 py-3 lg:py-3">
+            <Link href="/">
+                <a className="flex justify-self-center items-center">
+                    <img src={logo} width="150" height="150" alt="Colorchef" />
+                </a>
+            </Link>
+            <div className="block lg:hidden" ref={ref}>
+                <Burger open={show} setOpen={setShow} />
+            </div>
+            <div className="hidden lg:flex justify-end">
+                <Link href="/colors">
+                    <a
+                        className={`
 							text-sm mx-2 hover:text-violet-600 
-							${router.pathname === '/colors' ? 'text-violet-600' : 'text-gray-600'} 
+							${router.pathname === "/colors" ? "text-violet-600" : "text-gray-600"} 
 							${styles.navLink}
 						`}
-					>
-						Colors
-					</a>
-				</Link>
-				<Link href="/shadows">
-					<a
-						className={`
+                    >
+                        Colors
+                    </a>
+                </Link>
+                <Link href="/shadows">
+                    <a
+                        className={`
 							text-sm mx-2 hover:text-violet-600 
-							${router.pathname === '/shadows' ? 'text-violet-600' : 'text-gray-600'} 
+							${router.pathname === "/shadows" ? "text-violet-600" : "text-gray-600"} 
 							${styles.navLink}
 						`}
-					>
-						Shadows
-					</a>
-				</Link>
-				<Link href="/dark-palette">
-					<a
-						className={`
+                    >
+                        Shadows
+                    </a>
+                </Link>
+                <Link href="/dark-palette">
+                    <a
+                        className={`
 							text-sm mx-2 hover:text-violet-600 
-							${router.pathname === '/dark-palette' ? 'text-violet-600' : 'text-gray-600'} 
+							${router.pathname === "/dark-palette" ? "text-violet-600" : "text-gray-600"} 
 							${styles.navLink}
 						`}
-					>
-						Dark Palette
-					</a>
-				</Link>
-				<Link href="/buttons">
-					<a
-						className={`
+                    >
+                        Dark Palette
+                    </a>
+                </Link>
+                <Link href="/buttons">
+                    <a
+                        className={`
 							text-sm mx-2 hover:text-violet-600 
-							${router.pathname === '/buttons' ? 'text-violet-600' : 'text-gray-600'} 
+							${router.pathname === "/buttons" ? "text-violet-600" : "text-gray-600"} 
 							${styles.navLink}
 						`}
-					>
-						Buttons
-					</a>
-				</Link>
-				<Link href="/golden-ratio">
-					<a
-						className={`
+                    >
+                        Buttons
+                    </a>
+                </Link>
+                <Link href="/golden-ratio">
+                    <a
+                        className={`
 							text-sm mx-2 hover:text-violet-600 
-							${router.pathname === '/golden-ratio' ? 'text-violet-600' : 'text-gray-600'} 
+							${router.pathname === "/golden-ratio" ? "text-violet-600" : "text-gray-600"} 
 							${styles.navLink}
 						`}
-					>
-						Golden Ratio
-					</a>
-				</Link>
-			</div>
-			<div className={`hidden lg:block`}>
-				<Auth />
-			</div>
-		</nav>
-	);
+                    >
+                        Golden Ratio
+                    </a>
+                </Link>
+                <Link href="/about">
+                    <a
+                        className={`
+							text-sm mx-2 hover:text-violet-600 
+							${router.pathname === "/about" ? "text-violet-600" : "text-gray-600"} 
+							${styles.navLink}
+						`}
+                    >
+                        About
+                    </a>
+                </Link>
+            </div>
+            <div className={`hidden lg:block`}>
+                <Auth />
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,67 +1,43 @@
-import Meta from 'components/Meta';
-import Head from 'next/head';
-import React from 'react';
+import Header from "components/Header/Header";
+import Meta from "components/Meta";
+import Head from "next/head";
+import React from "react";
 
 const About = () => {
-	return (
-		<main className="container mx-auto px-4 md:px-10 lg:px-16">
-			<Head>
-				<title>About Colorchef</title>
-				<Meta
-					title="About Colorchef"
-					url="/about"
-					image={require('/images/hero.png')}
-					description="Colorchef is a product derived from the idea to solve the basic color-based problems faced by the UI/UX designers"
-				/>
-			</Head>
-			<div className="text-center py-3">
-				<h1 className={`text-3xl md:text-5xl`}>About Us</h1>
-			</div>
-			<div className={`w-full md:w-3/4 mx-auto px-8 md:px-20 font-body`}>
-				Colorchef is a product derived from the idea to solve the basic
-				color-based problems faced by the UI/UX designers which is
-				majorly selection of colors and color combinations. Two MSc
-				grads from Mumbai decide to work on this and hence, Colorchef
-				was created.
-			</div>
-			<h2 className="text-xl md:text-3xl text-center pt-4">Ping Us</h2>
-			<div className="flex flex-col md:flex-row w-3/4 mx-auto justify-around items-center md:items-start text-center py-3">
-				<div className="pb-3">
-					<div className={`text-lg md:text-xl font-head`}>
-						Linkedin
-					</div>
-					<a
-						className={`text-violet-600`}
-						target="_blank"
-						href="https://www.linkedin.com/in/siddheshnaik26/"
-						rel="noreferrer"
-					>
-						Siddhesh
-					</a>
-					<br />
-					<a
-						className={`text-violet-600`}
-						target="_blank"
-						href="https://www.linkedin.com/in/chinmaybhoir/"
-						rel="noreferrer"
-					>
-						Chinmay
-					</a>
-				</div>
-				<div className="pb-3">
-					<div className={`text-lg md:text-xl font-head`}>
-						Support
-					</div>
-					<a
-						className={`text-violet-600`}
-						href="mailto:hellocolorchef@gmail.com"
-					>
-						hellocolorchef@gmail.com
-					</a>
-				</div>
-			</div>
-		</main>
-	);
+    return (
+        <main className="container mx-auto px-4 md:px-10 lg:px-16">
+            <Head>
+                <title>About Colorchef</title>
+                <Meta
+                    title="About Colorchef"
+                    url="/about"
+                    image={require("/images/hero.png")}
+                    description="Colorchef is a product derived from the idea to solve the basic color-based problems faced by the UI/UX designers"
+                />
+            </Head>
+            <Header title={"About Us"} />
+            <div className={`w-full md:w-3/4 mx-auto px-8 md:px-20 font-body`}>
+                Colorchef is a product which combines multiple color-based
+                utilities. It helps indie developers, solopreneuers, UI/UX
+                designers to get started with their products efficiently without
+                wasting time on thinking about colors. If you are craving
+                colors, rest assured our chef has got you covered! If you have
+                any requests our chef can glady take that up, we are a ping
+                away!
+            </div>
+            <h2 className="text-xl md:text-3xl text-center pt-4">Ping Us</h2>
+            <div className="flex flex-col md:flex-row w-3/4 mx-auto justify-around items-center md:items-start text-center py-3">
+                <div className="pb-3">
+                    <a
+                        className={`text-violet-600`}
+                        href="mailto:hellocolorchef@gmail.com"
+                    >
+                        hellocolorchef@gmail.com
+                    </a>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default About;
